4ae6417292463689a10c8da4c8fc2340739afb63
Charles Becker <c.l.becker@outlook.com>
2022-05-13 12:49:12 -0700

    just so

--- main.R
+++ main.R
@@ -1,2 +1,11 @@
-# Eight module in the professional certificate series
-# save as main.R
+# Eighth module in the professional certificate series
+rm(list=ls())
+# install.packages("installr")
+# run in RGui
+# library(installr)
+# updateR()
+library(tidyverse)
+
+print("Hello, World!")
+urName <- readline("Your name? ")
+print(paste("Hello, ", urName ))
