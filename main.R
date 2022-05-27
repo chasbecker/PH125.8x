85c31a77c1eaeb0f84aa239d917ac51d95296a26
Charles Becker <c.l.becker@outlook.com>
2022-05-27 12:18:43 -0700

    comp chk pt 1

--- main.R
+++ main.R
@@ -204,7 +204,32 @@ bind_rows(guessing, height_cutoff) %>%
   geom_point()
 
 rm(list = ls())
-library( tidyverse )
-library(caret)
+library( tidyverse ) # includes dplyr
+library( lubridate )
+library(caret) # this is ML, right?
 library( dslabs )
-data( heights )
+data( reported_heights )
+
+dat <- mutate( reported_heights, date_time = ymd_hms( time_stamp)) %>%
+  filter( date_time >= make_date( 2016,01,25 ) & date_time < make_date( 2016, 02, 1 )) %>%
+  mutate( type = ifelse( day(date_time) == 25 & hour(date_time) == 8 & between( minute(date_time), 15, 30), "inclass", "online")) %>%
+  select( sex, type )
+
+class(dat$sex)
+# dat$sex is a character type.  convert to a factor type....
+
+y <- factor(dat$sex, c("Female", "Male"))
+x <- dat$type
+
+tclass <- data.frame( y, x )
+
+iclass <- tclass %>% filter( x == "inclass")
+mean(iclass$y == "Female")
+
+oclass <- tclass %>% filter( x == "online")
+mean(oclass$y == "Female")
+
+y_hat <- ifelse( tclass$x == "inclass", "Female", "Male") %>% 
+  factor(levels = levels(tclass$y)) 
+mean(y_hat==y)
+
