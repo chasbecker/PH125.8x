2906854f0fe77778835c5e95960651f1be417370
Charles Becker <c.l.becker@outlook.com>
2022-06-13 14:14:40 -0700

    This is sooooo hard!!!

--- main.R
+++ main.R
@@ -407,6 +407,7 @@ mean( (y_hat - test_set$son)^2 )
 rm(list=ls())
 library( tidyverse )
 library( caret )
+library( MASS )
 
 set.seed( 1, sample.kind = "Rounding" )
 n <- 100
@@ -433,3 +434,104 @@ rmse <- replicate( n, {
 rmse
 mean(rmse)
 sd(rmse)
+
+# Q2
+rm(list=ls())
+library( tidyverse )
+library( caret )
+library( MASS )
+
+set.seed( 1, sample.kind = "Rounding" )
+n <- c( 100, 500, 1000, 5000, 10000 )
+results <- sapply( n, function(t){
+  Sigma <- 9*matrix( c( 1.0, 0.5, 0.5, 1.0 ), 2, 2 )
+  dat <-  MASS::mvrnorm( t, c(69,69), Sigma ) %>%
+    data.frame() %>% setNames( c("x", "y") )
+  
+  
+  rmse <- replicate( 100, {
+    test_index <- createDataPartition( dat$y, times = 1, p = 0.5, list = FALSE )
+    train_set <- dat %>% slice( -test_index )
+    test_set <- dat %>% slice( test_index )
+    fit <- lm( y ~ x, data = train_set )
+    y_hat <- predict( fit, newdata = test_set )
+    sqrt( mean( (y_hat - test_set$y )^2 ))
+    })
+  c( avg = mean(rmse), sd = sd(rmse) )
+})
+
+results
+
+# Q4
+rm(list=ls())
+
+set.seed( 1, sample.kind = "Rounding" )
+n <- 100
+Sigma <- 9*matrix( c( 1.0, 0.95, 0.95, 1.0 ), 2, 2 )
+dat <-  MASS::mvrnorm( n = 100, c(69,69), Sigma ) %>%
+  data.frame() %>% setNames( c("x", "y") )
+
+set.seed( 1, sample.kind = "Rounding" )
+rmse <- replicate( 100, {
+    test_index <- createDataPartition( dat$y, times = 1, p = 0.5, list = FALSE )
+    train_set <- dat %>% slice( -test_index )
+    test_set <- dat %>% slice( test_index )
+    fit <- lm( y ~ x, data = train_set )
+    y_hat <- predict( fit, newdata = test_set )
+    sqrt( mean( (y_hat - test_set$y )^2 ))
+})
+mean(rmse)
+sd(rmse)
+
+# Q6
+
+rm(list=ls())
+set.seed(1, sample.kind = "Rounding")
+Sigma <- matrix( c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3 )
+dat <- MASS::mvrnorm( n = 100, c(0,0,0), Sigma ) %>%
+  data.frame() %>% setNames( c( "y", "x_1", "x_2") )
+cor(dat)
+
+test_index <- createDataPartition( dat$y, times = 1, p = 0.5, list = FALSE )
+train_set <- dat %>% slice( -test_index )
+test_set <- dat %>% slice( test_index )
+
+fit <- lm( y ~ x_1, data = train_set )
+y_hat <- predict( fit, newdata = test_set )
+sqrt( mean( (y_hat - test_set$y )^2 ))
+
+fit <- lm( y ~ x_2, data = train_set )
+y_hat <- predict( fit, newdata = test_set )
+sqrt( mean( (y_hat - test_set$y )^2 ))
+
+fit <- lm( y ~ x_1 + x_2, data = train_set )
+y_hat <- predict( fit, newdata = test_set )
+sqrt( mean( (y_hat - test_set$y )^2 ))
+
+# ?!?!?!?!?
+
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.95, 0.75, 0.95, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+  data.frame() %>% setNames(c("y", "x_1", "x_2"))
+
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+train_set <- dat %>% slice(-test_index)
+test_set <- dat %>% slice(test_index)
+
+fit <- lm(y ~ x_1, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2))
+
+fit <- lm(y ~ x_2, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2))
+
+fit <- lm(y ~ x_1 + x_2, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2))
+
+
+
+
