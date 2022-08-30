f8b6d93cbea711b3306b6d0e5b859fd70f249ded
Charles Becker <c.l.becker@outlook.com>
2022-08-30 13:19:11 -0700

    agosto

--- main.R
+++ main.R
@@ -744,6 +744,52 @@ polls_2008 %>% mutate( smooth = fit$y ) %>%
 
 polls_2008 %>% ggplot(aes(day, margin)) +
   geom_point() +
-  geom_smooth( color = "red", span = 0.15, method = "loess", method..args = list(degree=1))
+  geom_smooth( color = "red", span = 0.15, method = "loess", method.args = list(degree=1))
 
 
+total_days = diff(range(polls_2008$day))
+span <- 21/total_days
+
+fit <- loess( margin ~ day, degree = 1, span = span, data = polls_2008 )
+
+polls_2008 %>% mutate( smooth = fit$fitted ) %>%
+  ggplot( aes( day, margin)) +
+  geom_point( size = 3, alpha = 0.5, color = "grey") +
+  geom_line( aes( day, smooth), color = "red")
+
+# Comprehension Check: Smoothing
+
+# install.packages("pdftools")
+
+rm(list = ls())
+library(tidyverse)
+library(lubridate)
+library(purrr)
+library(pdftools)
+
+fn <- system.file( "extdata", "RD-Mortality-Report_2015-18-180531.pdf", package="dslabs")
+dat <- map_df(str_split(pdf_text(fn), "\n"), function(s){
+  s <- str_trim(s)
+  header_index <- str_which(s, "2015")[1]
+  tmp <- str_split(s[header_index], "\\s+", simplify = TRUE)
+  month <- tmp[1]
+  header <- tmp[-1]
+  tail_index <- str_which(s, "Total")
+  n <- str_count(s, "\\d+")
+  out <- c(1:header_index, which(n==1), which(n>=28), tail_index:length(s))
+  s[-out] %>%
+    str_remove_all("[^\\d\\s]") %>%
+    str_trim() %>%
+    str_split_fixed("\\s+", n=6) %>%
+    .[,1:5] %>%
+    as_data_frame() %>%
+    setNames(c("day", header)) %>%
+    mutate(month=month, day=as.numeric(day)) %>%
+    gather(year, deaths, -c(day, month)) %>%
+    mutate(deaths=as.numeric(deaths))
+}) %>%
+  # uses Spanish "Agosto" (AGO) instead of English "August" (AUG)
+  mutate(month = recode(month, "JAN"=1, "FEB"=2, "MAR"=3, "APR"=4, "MAY"=5, "JUN"=6,
+                      "JUL"=7, "AGO"=8, "SEP"=9, "OCT"=10, "NOV"=11, "DEC"=12)) %>%
+  mutate(date = make_date(year, month, day)) %>%
+  dplyr::filter(date <= "2018-05-01")
